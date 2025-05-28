b0d30249352d5b8522e897db3454fe32941cad5a
galiciaabraham <galicia.abraham@hotmail.com>
2025-05-27 21:45:40 -0600

    Installed new dependency to edit the tab icons.

--- my-app/app/(tabs)/_layout.tsx
+++ my-app/app/(tabs)/_layout.tsx
@@ -1,6 +1,8 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Entypo from '@expo/vector-icons/Entypo';
+ 
 export default function TabLayout() {
   return (
     <Tabs>
@@ -8,12 +10,18 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="home" size={size} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="NewEntry"
         options={{
           title: 'New Entry',
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="new-message" size={24} color="black" />
+          ),
         }}
       />
     </Tabs>
